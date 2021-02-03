eac23126483f6effb8c822439108d45f3131394d
Adil Khatri <adil009ik@gmail.com>
2021-02-03 15:41:31 +0530

    added a to b conversion

---
+++ LCS and its variations/4. minimum_no_of_insertion_and_deletion_to_convert_a_to_b.py
@@ -0,0 +1,30 @@
+def LCSLengthIter(x, y, n, m):
+    cache = [[0 for i in range(m+1)] for i in range(n+1)]
+
+    for i in range(m+1):
+        cache[0][i] = 0
+
+    for j in range(n+1):
+        cache[j][0] = 0
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if x[i-1] == y[j-1]:
+                cache[i][j] = 1 + cache[i - 1][j - 1]
+            else:
+                cache[i][j] = max(cache[i-1][j], cache[i][j-1])
+
+    return cache[n][m]
+
+
+if __name__ == '__main__':
+    x = 'heap'
+    y = 'pea'
+    n = len(x)
+    m = len(y)
+
+    # Iterative
+    lcs_len = LCSLengthIter(x, y, n, m)
+    insertion = m - lcs_len
+    deletion = n - lcs_len
+    print('insertions', insertion, 'deletions', deletion)
